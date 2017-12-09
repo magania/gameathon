66e46b8c11005b59ee017eeca8e033169487381e
Ricardo Magana <ricardo.magana@wizeline.com>
2017-12-09 15:55:35 -0600

    miner

--- gameathon/miner.py
+++ gameathon/miner.py
@@ -1,24 +1,71 @@
 import Pyro4
+import hashlib
+import random
+import sys
+import time
+import requests
+
 from messenger import register
+
+def hashcash(msg):
+    m = hashlib.sha256()
+    m.update(msg)
+    return hashlib.sha256(m.digest()).digest()
+
 
 @Pyro4.expose
 class Miner(object):
+    _MAX_INT = 99999999999999999
+    _GAME = 'testnet'
+    _ENDPOINT = 'https://gameathon.mifiel.com/api/v1/games/{}/block_found' \
+                .format(_GAME)
+
     def __init__(self):
-        self.target = None
-        pass
+        self.target_raw = b'0'
+        self.target = 0
+        self.stop_ = True
 
     def stop(self):
-        pass
+        self.stop_ = True
+
+
+    def _report_found(self, block):
+        report = {
+              'prev_block_hash': block['prev_block_hash'],
+              'message': 'TeamZero Level Up!',
+              'nonce': block['nonce'],
+              'nickname': 'TeamZero',
+              'merkle_root': block['merkle_hash'],
+              'used_target': block['target'],
+              'transactions': block['transactions']
+            }
+
 
     def mine_block(self, block):
-        print('Mining {}'.format(block))
-        pass
+        partial_header = block['version'] + b'|' + \
+                         block['prev_block_hash'] + b'|' + \
+                         block['merkle_hash'] + b'|' + \
+                         self.target_raw + b'|' + \
+                         block['message'] + b'|'
+        self.stop_ = False
+        while not self.stop_:
+            nonce = random.randint(0, self._MAX_INT)
+            block_header = partial_header + str(nonce).encode('ascii')
+            hash_ = hashcash(block_header)
+            if int.from_bytes(hash_, byteorder='big') < self.target:
+                print('block_found!!! :')
+                print(block_header)
+                block['nonce'] = nonce
+                block['created_at'] = time.time()
+                block['target'] = self.target_raw
+                self._report_found(block)
+                self.stop_ = True
+
+
 
     def target_changed(self, new_target):
-        self.target = new_target
-
-    def listen(self):
-        pass
+        self.target_raw = new_target
+        self.target = int(new_target, 16)
 
 
 if __name__ == '__main__':
---
+++ gameathon/test_miner.py
@@ -0,0 +1,12 @@
+from miner import Miner
+
+from binascii import unhexlify
+
+mm = Miner()
+mm.target_changed(b'00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+block = {'version': b'1',
+         'prev_block_hash': b'dcc7d4fd08cbc0484f3f2eda3cc20f2b2d1b4e5eebfaafc82d5526d73637ff49',
+         'merkle_hash': b'dcc7d4fd08cbc0484f3f2eda3cc20f2b2d1b4e5eebfaafc82d5526d73637ff49',
+         'message': b'test message',
+         'transactions': []}
+mm.mine_block(block)
